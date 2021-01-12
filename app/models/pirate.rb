d83bf035506acc0cf3b73ecee0cae54fa04ef77b
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-12 11:54:22 -0600

    commit tree 7adfd11dad670121d66ccc96a6502eb64be35f27

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
-class Pirate
+class Pirate < Sinatra::Base
+    attr_accessor :name, :weight,:height 
+
+    def initialize(name, weight, height)
+        @name = name
+        @weight = weight
+        @height = height
+    end
 end
